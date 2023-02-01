21a087949d39ad1a631543e3d69546a39eb842bf
Randumb <78370387+RandumbGuy1@users.noreply.github.com>
2023-02-01 13:03:17 -0500

    2/1

--- Zack Lee Clicker Game/TestPythonProgram.py
+++ Zack Lee Clicker Game/TestPythonProgram.py
@@ -1,4 +1,5 @@
 import tkinter
+import asyncio
 from tkinter import ttk
 from PIL import ImageTk, Image  
 
@@ -20,9 +21,13 @@ class Player:
 #Create player
 player = Player()
 
+def TranslateLinkToZackTitle(link):
+  split1 = link.split("-")
+  split2 = split1[1].split(".")
+  return split2[0]
+
 #Create library of Zack Lee evolutions
-evolutions = ["ZackLee-Baby.webp", "ZackLee-Kid.png", "ZackLee-Teen.webp", "ZackLee-Adult.webp", "ZackLee-Trained.jpg", "ZackLee-God.png", "ZackLee-Real.png"]
-evolutionTitles = ["Baby", "Kid", "Teen", "Adult", "Trained", "God", "Real"]
+evolutions = ["ZackLee-Baby.webp", "ZackLee-Kid.png", "ZackLee-Teen.webp", "ZackLee-RedGlasses.jpg", "ZackLee-RizzGod.webp", "ZackLee-Trained.jpg", "ZackLee-Insane.jpeg", "ZackLee-God.png", "ZackLee-BabyEater.jpg", "ZackLee-Real.png"]
 
 #Create fullscreen TK window
 window = tkinter.Tk()
@@ -37,18 +42,37 @@ title.pack(padx=30, pady=30)
 level = tkinter.Label(text="Level: " + str(player.level) + ", Click Goal: " + str(player.xp_goal), fg="black", bg="white")
 level.pack(padx=10, pady=10)
 
-#Create button and add style
+#Create button style
 style = ttk.Style()
 style.map("C.TButton",
     foreground=[('pressed', 'red'), ('active', 'black')],
     background=[('pressed', 'yellow'), ('active', 'white')]
     )
 
-button = ttk.Button(window, text="\n\n Click to start your Zack Lee Journey \n\n", style="C.TButton", width=70)
+#Resgister button with click input and style
+def onClick():
+  global zackImage
+  global zackLabel
+  global prev_level
+
+  player.registerClick(button)
+  button.configure(text= "\n\n" + str(player.click_count) + "\n\n")
+
+  if player.level != prev_level:
+    level.configure(text="Level: " + str(player.level) + ", Click Goal: " + str(player.xp_goal))
+    current.configure(text="You are currently: " + TranslateLinkToZackTitle(evolutions[player.level - 1]) + " Zack Lee")
+
+    zackImage = ImageTk.PhotoImage(Image.open("Zack_lee_Evolutions/" + evolutions[player.level - 1]))
+    zackLabel = tkinter.Label(image=zackImage)
+    zackLabel.place(x=850, y=350)
+
+    prev_level = player.level
+
+button = ttk.Button(window, text="\n\n Click to start your Zack Lee Journey \n\n", style="C.TButton", width=70, command=onClick)
 button.pack(padx=10, pady=10)
 
 #Display current zack lee evolution title
-current = tkinter.Label(text="You are currentely: " + evolutionTitles[player.click_count] + " Zack Lee", fg="black", bg="white")
+current = tkinter.Label(text="You are currentely: " + TranslateLinkToZackTitle(evolutions[player.level - 1]) + " Zack Lee", fg="black", bg="white")
 current.pack(padx=10, pady=10)
 
 #Put up starting image of Zack Lee
@@ -58,24 +82,24 @@ zackLabel = tkinter.Label(image=zackImage)
 zackLabel.place(x=850, y=350)
 
 prev_level = player.level
-#Resgister button click input
-def onClick(event):
-  global zackImage
-  global zackLabel
-  global prev_level
 
-  player.registerClick(button)
-  button.configure(text= "\n\n" + str(player.click_count) + "\n\n")
+#Autoclicking check loop
+elapsed = 0
+async def registerAutoClick():
+  global elapsed
+  
+  while (True):
+    elapsed += 1
+    if auto_click.get() and elapsed > 100 / player.xp_goal:
+      onClick()
+      elapsed = 0
 
-  if (player.level != prev_level):
-    level.configure(text="Level: " + str(player.level) + ", Click Goal: " + str(player.xp_goal))
-    current.configure(text="You are currently: " + evolutionTitles[player.level - 1] + " Zack Lee")
+    window.update()
+    await asyncio.sleep(0.001)
 
-    zackImage = ImageTk.PhotoImage(Image.open("Zack_lee_Evolutions/" + evolutions[player.level - 1]))
-    zackLabel = tkinter.Label(image=zackImage)
-    zackLabel.place(x=850, y=350)
+#Checkbox
+auto_click = tkinter.BooleanVar()
+c1 = tkinter.Checkbutton(window, text='AutoClick',variable=auto_click, onvalue=True, offvalue=False)
+c1.pack()
 
-    prev_level = player.level
-
-button.bind("<ButtonRelease-1>", onClick)
-window.mainloop()
+asyncio.run(registerAutoClick())
